6436f38565a15aefb79d35528da078629dc5252f
Moise-code <moisewowe@gmail.com>
2022-10-19 20:23:33 +0200

    Adding json file to dynamicaly change the singers section

---
+++ main.js
@@ -0,0 +1,53 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable quotes */
+const hamburger = document.querySelector(".hamburger");
+const navLists = document.querySelector(".nav-lists");
+const singerContentContainer = document.querySelector(
+  ".singer-content-container"
+);
+
+hamburger.addEventListener("click", (e) => {
+  e.preventDefault();
+  hamburger.classList.toggle("active");
+  navLists.classList.toggle("active");
+});
+document.querySelectorAll(".nav-link").forEach((nav) =>
+  // eslint-disable-next-line implicit-arrow-linebreak
+  nav.addEventListener("click", (e) => {
+    e.preventDefault();
+    hamburger.classList.remove("active");
+    navLists.classList.remove("active");
+  })
+);
+
+const displaySingers = ({name,about,paragraph,image}) => {
+  let div = document.createElement("div");
+  div.className = "singer-content-content";
+  div.innerHTML = `
+    <div class="singer-content-image">
+    <img src="${image}" alt="${name}">
+</div>
+<div class="side-singer-content">
+    <h3 class="singer-title">${name}</h3>
+    <p class="about-singer">${about}</p>
+    <hr>
+    <p class="singer-paragraph">${paragraph}</p>
+</div>
+    `;
+    return div;
+};
+
+const getSingers = async () => {
+  const response = await fetch("./singers.json");
+
+  try {
+    const data = await response.json();
+
+    data.forEach((singer) => {
+      singerContentContainer.append(displaySingers(singer));
+    });
+  } catch (error) {
+    // console.log(error);
+  }
+};
+getSingers();
